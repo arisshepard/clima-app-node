59f196ee05b83a2b1935445d49c89e12df203500
Maria <mfiremetal@gmail.com>
2020-11-15 10:58:19 +0100

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Aplicación del Clima - Curso Node
+
+Recuerda ejecutar `npm install` para las librerías
+
+## Ejemplo
+
+```
+node app.js -d "Madrid"
+```
---
+++ app.js
@@ -0,0 +1,32 @@
+const lugar = require('./lugar/lugar');
+const argv = require('./config/yargs').argv;
+const clima = require('./clima/clima');
+
+// console.log(argv.direccion);
+
+// lugar
+// 	.getLugarLatLng(argv.direccion)
+// 	.then((resp) => console.log(resp))
+// 	.catch((err) => console.log('Error!!!', err));
+
+// clima
+// 	.getClima(35, 139)
+// 	.then((resp) => console.log('La temperatura: ', resp))
+// 	.catch((err) => console.log('ERROR!!!!', err));
+
+const getInfo = async (direccion) => {
+	// conseguimos el lugar
+	const coordenadas = await lugar.getLugarLatLng(argv.direccion);
+
+	// conseguimos la temperatura
+	const temperatura = await clima.getClima(coordenadas.lat, coordenadas.lng);
+
+	if (!temperatura) {
+		throw new Error(`No se pudo determinar el clima de ${direccion}`);
+	}
+
+	// salida
+	return `El clima de ${direccion} es de ${temperatura}`;
+};
+
+getInfo(argv.direccion).then(console.log).catch(console.log);
